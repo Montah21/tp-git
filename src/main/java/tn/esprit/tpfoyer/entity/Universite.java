0c7d99870cfd4e86e4cea3bb87ed9eca484fe526
Montah21 <moutye.bacha@esprit.tn>
2024-11-04 15:51:48 +0100

    Add test class for UniversiteServiceImpl

--- src/main/java/tn/esprit/tpfoyer/entity/Universite.java
+++ src/main/java/tn/esprit/tpfoyer/entity/Universite.java
@@ -24,6 +24,18 @@ public class Universite {
     @OneToOne(cascade = CascadeType.ALL)
     Foyer foyer;
 
+    // You can remove these if they aren't needed
+    // public Universite(long idUniversite, String nomUniversite) {
+    //     this.idUniversite = idUniversite;
+    //     this.nomUniversite = nomUniversite;
+    // }
+
+    // Optionally remove these as well if not needed
+    // public void setId(long idUniversite) {
+    //     this.idUniversite = idUniversite;
+    // }
+
+    // public void setNom(String nomUniversite) {
+    //     this.nomUniversite = nomUniversite;
+    // }
 }
-
-
